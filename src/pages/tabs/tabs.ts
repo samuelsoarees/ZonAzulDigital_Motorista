fa0577bc9349d96adc8107d45acd8d6029c61805
Cadu Castanha <caducastanha@gmail.com>
2017-12-10 12:24:07 -0300

    resolvendo conflitos

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { CompraPage } from '../compra/compra';
 import { HistoricoPage } from '../historico/historico';
 import { AtivoPage } from '../ativo/ativo';
+import { PerfilPage } from '../perfil/perfil';
 
 @Component({
   templateUrl: 'tabs.html'
@@ -12,6 +13,7 @@ export class TabsPage {
   tab1Root = AtivoPage;
   tab2Root = CompraPage;
   tab3Root = HistoricoPage;
+  tab4Root = PerfilPage;
 
   constructor() {
 
